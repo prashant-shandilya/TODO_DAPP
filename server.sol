e46467040016b75fe55a9e75c7cf2e015ac6327c
Prashant Shandilya <91403042+vikalp999@users.noreply.github.com>
2022-05-01 18:14:50 +0530

    Add files via upload

---
+++ server.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList{
+
+    uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+    constructor() {
+        createTask("Check out Dapp Uni at youtube");
+    }
+
+    mapping(uint=>Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id,_task.completed);
+    }
+    
+}
